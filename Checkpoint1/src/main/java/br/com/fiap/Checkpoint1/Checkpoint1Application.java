76724d649a2515c14167a908dc738d2ba39671ff
santos-nikolas <126473396+santos-nikolas@users.noreply.github.com>
2025-04-23 09:37:17 -0300

    Adicionando código gerado com springboot

---
+++ Checkpoint1/src/main/java/br/com/fiap/Checkpoint1/Checkpoint1Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Checkpoint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoint1Application.class, args);
+	}
+
+}
